e483cc084fbe3dd05ac79b49794b002896e68e30
yannick818 <yannick.stnr+github@gmail.com>
2024-04-30 23:22:06 +0200

    init askama axum example

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use askama_axum::Template;
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+
+#[derive(Template)]
+#[template(path = "index.html")]
+pub struct MyTemplate {}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+
+        .route("/", get(handle_main));
+
+    let listener = tokio::net::TcpListener::bind("localhost:3000").await.unwrap();
+    println!("listening on {}", listener.local_addr().unwrap()); 
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handle_main() -> impl IntoResponse {
+    let template = MyTemplate {};
+    let reply_html = template.render().unwrap();
+    (StatusCode::OK, Html(reply_html).into_response())
+}

ff5e698e7bd718c98915ecce3cbebf9c703d44ca
yannick818 <yannick.stnr+github@gmail.com>
2024-05-02 11:35:13 +0200

    cleanup Readme update

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Readme
+
+Intro from [Primagen Introduction]<https://youtu.be/x7v6SNIgJpE?si=Lj4gsmviABn8LEtC>
+The video is an HTMX introduction that uses Go as backend.
+This is a version using an Axum Rust backend.
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,18 @@
 use askama_axum::Template;
 use axum::{
-    http::StatusCode,
-    response::{Html, IntoResponse},
     routing::get,
     Router,
 };
 
 #[derive(Template)]
 #[template(path = "index.html")]
-pub struct MyTemplate {}
+pub struct IndexHtml {}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-
-        .route("/", get(handle_main));
+    let app = Router::new().route("/", get(|| async {IndexHtml{}}));
 
     let listener = tokio::net::TcpListener::bind("localhost:3000").await.unwrap();
     println!("listening on http://{}", listener.local_addr().unwrap()); 
     axum::serve(listener, app).await.unwrap();
-}
-
-async fn handle_main() -> impl IntoResponse {
-    let template = MyTemplate {};
-    let reply_html = template.render().unwrap();
-    (StatusCode::OK, Html(reply_html).into_response())
 }
